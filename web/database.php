b5326cff9c0f2c10109074e0d9e5223bebfdb3b2
STPSUN <497432519@qq.com>
2018-12-18 13:52:18 +0800

    1

--- web/database.php
+++ web/database.php
@@ -8,7 +8,7 @@ return [
 //    'hostname'        => '127.0.0.1',
     // 数据库名
 //    'database'        => 't4d',
-    'database'        => 'ddopr',
+    'database'        => 'ddorp',
     // 用户名
 //    'username'        => 'root',
     'username'        => 'ddopr',
