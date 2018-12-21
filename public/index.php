a3ad964806df896b34714ff9ab5a95abb0a6adb9
STPSUN <497432519@qq.com>
2018-12-21 17:42:00 +0800

    1

--- public/index.php
+++ public/index.php
@@ -18,9 +18,9 @@ define('ADDONS_PATH', __DIR__ . '/../addons/');
 //上传目录
 define('UPLOADFOLDER', './uploads/');
 $url = $_SERVER['SERVER_NAME'];
-//if($url != 'www.ddpor.vip2')
+if($url != 'www.ddpor.vip')
 //if($url != 't4d.com')
-//    exit();
+    exit();
 
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
