ed682a96a07a6dd970458a3a5da212016285de1b
STPSUN <497432519@qq.com>
2018-12-26 10:07:03 +0800

    1

--- web/index/controller/Index.php
+++ web/index/controller/Index.php
@@ -7,14 +7,8 @@ namespace web\index\controller;
  */
 class Index extends Base {
 
-    public function index()
-    {
-        $m = new \addons\eth\user\controller\Crontab();
-        $m->clientEthTosys();
-    }
-    
     //定位到fomo游戏界面
-    public function index2(){
+    public function index(){
 //        return header('Location: http://mobile.luckywinner.vip/app');
         $inviter_code = $this->_get('inv');
         session('inviter_code',$inviter_code);//设置邀请者地址缓存
